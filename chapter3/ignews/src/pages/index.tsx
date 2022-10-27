5f429ebfec2f496e02a9154cfab38f85cbd50258
LeonardoDuarteBonafe <32874289+LeonardoDuarteBonafe@users.noreply.github.com>
2022-10-26 23:26:49 -0300

    adding stripe function and configuring it

--- chapter3/ignews/src/pages/index.tsx
+++ chapter3/ignews/src/pages/index.tsx
@@ -1,9 +1,19 @@
+import {GetServerSideProps} from 'next'
 import Head from 'next/head'
+
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stipe'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number,
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -15,13 +25,31 @@ export default function Home() {
           <h1>News about the <span>React</span>world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>from $9.90 month</span>
+            <span>from {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"/>
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1LxL8sDUUy9gwrr0G3p3VsBd')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
 }

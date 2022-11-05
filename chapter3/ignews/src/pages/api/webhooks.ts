ee1ab4098b82de121266cb417e8fc9dcb989feb0
LeonardoDuarteBonafe <32874289+LeonardoDuarteBonafe@users.noreply.github.com>
2022-11-04 21:07:40 -0300

    burling the system

--- chapter3/ignews/src/pages/api/webhooks.ts
+++ chapter3/ignews/src/pages/api/webhooks.ts
@@ -3,5 +3,5 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default (req: NextApiRequest, res: NextApiResponse) => {
     console.log('evento recebido ')
 
-    res.status(200).json({ok: true})
+    res.status(200).json({ok: true}) //cheating
 }
